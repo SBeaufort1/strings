9476d3a705bbce04560cf1d1fb91a33cd6695de9
sheba beaufort <shebabeaufort@gmail.com>
2023-06-01 15:03:33 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//let x=15;
+//console.log(x);
+
+//let a=7
+//console.log(a)
+
+//let names=["jim", "james", "paul", "tyrone"];
+//console.log(names);
+
+//let seed1="carrots";
+//console.log(seed1);
+
+//let vegetables=20;
+//console.log(vegetables);
+
+//let x=28;
+//console.log(x);
+
+// you so awesome girl!
+
+let x=12;
+let a=78;
+
+if (x ===12 && a ===78) {
+    let sum=x+a;
+    console.log(sum);
+}
+
+
