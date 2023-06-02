cec23a3f71bbfcbafb85da13e2490cfee663be78
sheba beaufort <shebabeaufort@gmail.com>
2023-06-02 14:53:19 -0400

    second commit/if statement

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// *Ex. 1
+
 //let x=15;
 //console.log(x);
 
 //let a=7
 //console.log(a)
 
+// *Ex. 2
+
 //let names=["jim", "james", "paul", "tyrone"];
 //console.log(names);
+
+// *Ex. 3
 
 //let seed1="carrots";
 //console.log(seed1);
@@ -13,17 +19,138 @@
 //let vegetables=20;
 //console.log(vegetables);
 
+// *Ex. 4
+
 //let x=28;
 //console.log(x);
 
-// you so awesome girl!
+//let x=12;
+//let a=78;
 
-let x=12;
-let a=78;
+// *Ex. 5 equa; signs ===
 
-if (x ===12 && a ===78) {
-    let sum=x+a;
-    console.log(sum);
+//if (x ===12 && a ===78) {
+//   let sum=x+a;
+//   console.log(sum);
+//}
+
+// *Ex. 6 begin if and else statment
+
+//let age = 18;
+//if (age>= 18) {
+    //do this
+//    console.log("access granted");
+//}
+//else{
+    //do this
+//    console.log("access denied");
+//}
+
+// *Ex. 7 
+
+//let wood = 39;
+//if (wood>= 40) {
+    // we have enough for a treehouse
+//    console.log("great job");
+//}
+//else{
+    //to bad so sad
+//    console.log("Not enough wood");
+//}
+
+// *Ex. 7 
+
+// let username = "admin";
+// let password = "Pass614";
+
+// if (password === "Pass614" && username === "admin") {
+//     //correct
+//     console.log("Access Granted");   
+// }
+// else{
+//     //incorrect
+//     console.log("Access Denied");  
+// }
+
+// *Ex. 8 Begin else if.
+
+// let wood = 39;
+// if (wood>= 40) {
+//     //we have enough for a treehouse
+//    console.log("you can build a treehouse")
+
+// }
+// else if (wood >=20){
+//     //to bad so sad
+//    console.log("build a stool");
+// }
+
+// else{
+//     console.log("you are s.o.l");
+// }
+
+//  *Ex. write a nested if/else statement declare a variable 'number' 
+// add an if/else statement that check if 'number' is positive & greater than 100
+// add another statement that checks if 'number' is positive but less than 100
+// add an the final statement to check if 'number' is positive
+
+// let number= 40;
+// if (number>= 100) {
+//    console.log("Number is positive & greater than 100")
+// }
+
+// else if (number<= 100){ 
+//    console.log("Number is positive but less than 100");
+// }
+
+// else {
+//     console.log("Number is negative");
+// }
+
+// *need to redo to make changes for better outcome (homework)
+
+// let number= 100;
+// if (number>0 && number>100) {
+//    console.log("Number is positive & greater than 100");  
+// }
+   
+
+// else if (number>0 && number<100){ 
+//    console.log("Number is positive but less than 100");
+// }
+
+// else {
+//     console.log("Number is negative");
+// }
+
+// *week 2 -Day 3-lab
+
+// write an if/else statement for the following requirements:
+// if a student gets 80 or higher: console log 'you did a great job!'
+// if a student gets 70 or above: console log 'keep trying'
+// if a student gets 60 or above: console log 'ehhhh'
+// if a student gets 55 or above: console log 'Not good enough'
+// any grade lower than 55 is 'Bad grade but You'll get em next time'
+
+let number= 75;
+
+if (number>= 80 ) {
+    console.log("you did a great job!");  
+ }
+   
+else if (number>= 70){ 
+   console.log("keep trying");
 }
 
+else if (number>= 60){ 
+    console.log("ehhhh");
+ }
 
+ else if (number>= 55){ 
+    console.log("Not good enough");
+ }
+
+ else {
+    console.log("Bad grade but You'll get em next time");
+ }
+ 
